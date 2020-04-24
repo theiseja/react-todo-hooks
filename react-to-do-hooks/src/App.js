1ec972905f22c56ea577e6e0045ea0ac36e983d2
Jesse Theisen <theiseja@gmail.com>
2020-04-23 21:22:22 -0500

    Starts building todo app

---
+++ react-to-do-hooks/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  // const completeTodo = index => {
+  //   const newTodos = [...todos];
+  //   newTodos[index].isCompleted = true;
+  //   setTodos(newTodos);
+  // };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn about React",
+      isCompleted: false },
+    { text: "Meet friend for lunch",
+      isCompleted: false },
+    { text: "Build really cool todo app",
+      isCompleted: false }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+  export default App;
